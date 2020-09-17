59f212f464c881084755311e87cb8124599c0aea
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-17 21:30:29 +0200

    added clarifai npm package to the project so that we can interact with the api, got an api key and added some code to be able to make an api call to clarifai

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo';
@@ -74,18 +75,49 @@ const particlesOptions = {
     "retina_detect": true
   };
 
-function App() {
-  return (
-    <div className="App">
-       <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    {
-    /* <FacRecognition /> */}
-    </div>
-  );
+  const app = new Clarifai.App({
+    apiKey: 'cb525549334c4b708f70f45e4a2db83b',
+  })
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state= {
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onClickDetectButton = (event) => {
+    app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log('There is an error', error);
+    });
+  }
+  // api key cb525549334c4b708f70f45e4a2db83b
+  
+  render() {
+    return (
+      <div className="App">
+         <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onClickDetectButton={this.onClickDetectButton} 
+        />
+      {
+      /* <FacRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageFormLink = () => {
+const ImageFormLink = (props) => {
     return (
         <div className="f3">
             <p>{'Hey there. This Brain will detect faces in images you provide it. Give it a try.'}</p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input className='f4 pa2 w-70 center' type="tex"></input>
-                    <button className='w-30 grow f4 pa2 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                    <input onChange={props.onInputChange} className='f4 pa2 w-70 center' type="tex"></input>
+                    <button onClick={props.onClickDetectButton} className='w-30 grow f4 pa2 link ph3 pv2 dib white bg-light-purple'>Detect</button>
                 </div>
                 
             </div>
