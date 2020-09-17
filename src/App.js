3bcee02436388fd8d0d514d1fa9366f8a181df27
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-17 15:06:36 +0200

    added all the components we need without any functionality and added a bunch of cool styling libraries

--- src/App.js
+++ src/App.js
@@ -1,16 +1,89 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+      },
+      "opacity": {
+        "value": 0.4406627567862068,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 0,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "retina_detect": true
+  };
 
 function App() {
   return (
     <div className="App">
+       <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
     {
-      /*<ImageLinkForm />
-    <FacRecognition /> */}
+    /* <FacRecognition /> */}
     </div>
   );
 }
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageFormLink = () => {
+    return (
+        <div className="f3">
+            <p>{'Hey there. This Brain will detect faces in images you provide it. Give it a try.'}</p>
+            <div className="center">
+                <div className="form center pa4 br3 shadow-5">
+                    <input className='f4 pa2 w-70 center' type="tex"></input>
+                    <button className='w-30 grow f4 pa2 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                </div>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default ImageFormLink;
