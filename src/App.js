a3e6ec9a91c7737d36b732ead47e0b45de24a47c
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-27 21:22:32 +0200

    added the facial recognition react component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank';
@@ -16,24 +17,26 @@ class App extends React.Component {
   constructor() {
     super();
     this.state= {
-      input:'',
+      input: '',
+      imageUrl: '',      
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value)
+    this.setState({input: event.target.value});
   }
 
   onClickDetectButton = (event) => {
-    app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
+    this.setState({imageUrl: this.state.input});
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
     .then(response => {
-      console.log(response);
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
     })
     .catch(error => {
       console.log('There is an error', error);
     });
   }
-  // api key cb525549334c4b708f70f45e4a2db83b
   
   render() {
     return (
@@ -46,8 +49,7 @@ class App extends React.Component {
         onInputChange={this.onInputChange} 
         onClickDetectButton={this.onClickDetectButton} 
         />
-      {
-      /* <FacRecognition /> */}
+       <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
