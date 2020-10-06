07de8c67de66d0f6c06faf6e1572c212d361107c
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-10-06 14:05:52 +0200

    added nodemon that watches for changes to the script file

--- NodeBasics/script1.js
+++ NodeBasics/script1.js
@@ -2,12 +2,10 @@
 const script2 = require('./script2');
 //built in modules
 
-const fileSystem = require('fs');
-const fileSystem = require('http');
+// const fileSystem = require('fs');
+// const fileSystem = require('http');
 
 const a = script2.largeNumber;
-const b = 6;
+const b = 10;
 
 console.log(a+b);
-
-console.log(fileSystem);
