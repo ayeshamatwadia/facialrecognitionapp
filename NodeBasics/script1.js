362ba7d587d8d939628a290c480a4e4f56d7a695
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-10-06 14:00:45 +0200

    using a built in module

--- NodeBasics/script1.js
+++ NodeBasics/script1.js
@@ -1,6 +1,13 @@
 // importing method commonjs modules
 const script2 = require('./script2');
+//built in modules
+
+const fileSystem = require('fs');
+const fileSystem = require('http');
+
 const a = script2.largeNumber;
 const b = 6;
 
 console.log(a+b);
+
+console.log(fileSystem);
