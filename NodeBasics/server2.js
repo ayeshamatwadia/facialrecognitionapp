dc0468985f195121984f728e979a3954fa501417
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-11-09 18:47:40 +0200

    added the different things you can access on the request

--- NodeBasics/server2.js
+++ NodeBasics/server2.js
@@ -5,22 +5,15 @@ const app = express();
 // express middleware which can change the request before
 // getting the routes
 
-app.use((req, res, next) => {
-    console.log("hello");
-    next();
-})
-
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.get('/profile', (req, res) => {
+    req.query;
+    req.body;
+    req.header;
+    req.params;
     res.send("getting the profile");
 });
 
-
-app.post('/profile', (req, res) => {
-    console.log('the request is ', req.body);
-    res.send('test');
-});
-
-app.listen(3000);
+app.listen(3001);
