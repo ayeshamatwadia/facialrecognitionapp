4887cced7f2bccbf99a880be743fa625958b49a2
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-12-09 20:31:18 +0200

    added the endpoint code and a little database variable to keep track of users

--- facial-app-api/server.js
+++ facial-app-api/server.js
@@ -1,9 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/', (req, resp) => {
-    resp.send('this is working');
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Ayesha',
+            email: 'ayesha@test.com',
+            password: 'cookie',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Nosh',
+            email: 'nosh@test.com',
+            password: 'cookie2',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.json(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    const {email, password} = req.body;
+    if(email === database.users[0].email && password === database.users[0].password){
+        res.json('success');
+    } else {
+        res.json('failed');
+    }
+})
+
+app.post('/register', (req, res) => {
+  const {email, name, password} = req.body;
+  database.users.push(
+        {
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+        }
+    )
+    res.json(database.users[database.users.length -1]);
 })
 
 app.listen(3000, () => {
